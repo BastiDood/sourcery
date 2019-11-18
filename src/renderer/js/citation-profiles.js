c47a58ce39b3f4559b0ac171f9dac020b04f7cd2
Basti Ortiz <ortizbasti16@gmail.com>
2019-11-19 00:33:51 +0800

    Add partial profile for journal and periodical articles
    No implementation logic yet

--- src/renderer/js/citation-profiles.js
+++ src/renderer/js/citation-profiles.js
@@ -8,6 +8,19 @@ const COMMON_PROFILE = [
 ];
 
 const PROFILES = {
+  JOURNAL_PERIODICAL_ARTICLE: [
+    ...COMMON_PROFILE,
+    'publisherName',
+    'publisherCountry',
+    'publisherStateProvince',
+    'publisherCity',
+    'url',
+    'doi',
+    'volume',
+    'issue',
+    'startPage',
+    'endPage'
+  ],
   WEB_RESOURCE: [
     ...COMMON_PROFILE,
     'publisherName',
